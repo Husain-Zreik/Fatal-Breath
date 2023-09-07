8f78c86fecf8695517eaf8e7c3cc799fee6e8d8a
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-07 23:51:53 +0300

    Frontend <> created remote config file

---
+++ fatal_breath_frontend/lib/config/remote.config.dart
@@ -0,0 +1,44 @@
+import 'package:fatal_breath_frontend/config/local.storage.config.dart';
+import 'package:fatal_breath_frontend/enums/request.methods.dart';
+import 'package:fatal_breath_frontend/enums/local.types.dart';
+import 'package:dio/dio.dart';
+
+final options = BaseOptions(
+  baseUrl: "http://192.168.0.103:8000",
+  contentType: Headers.jsonContentType,
+);
+
+final dioClient = Dio(options);
+
+Future sendRequest(
+    {required String route,
+    RequestMethods method = RequestMethods.GET,
+    Map? load}) async {
+  final String? token =
+      await getLocal(type: LocalTypes.String, key: "access_key");
+
+  if (token != null) {
+    final BaseOptions authorizeOptions =
+        dioClient.options.copyWith(headers: {"Authorization": "Bearer $token"});
+
+    dioClient.options = authorizeOptions;
+  }
+
+  if (method == RequestMethods.GET) {
+    final response = await dioClient.get(route, data: load);
+    return response;
+  } else if (method == RequestMethods.POST) {
+    final response = await dioClient.post(route, data: load);
+    return response;
+  } else if (method == RequestMethods.DELETE) {
+    final response = await dioClient.delete(route, data: load);
+    return response;
+  } else if (method == RequestMethods.PUT) {
+    final response = await dioClient.put(route, data: load);
+    return response;
+  }
+
+  return Future(() {
+    return "NULL";
+  });
+}
