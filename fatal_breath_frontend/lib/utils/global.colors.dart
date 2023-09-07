3c22acbe1a8f22dcbbcdb61ed9c10c2de53c04bf
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-07 23:52:39 +0300

    Frontend <> added error text color

--- fatal_breath_frontend/lib/utils/global.colors.dart
+++ fatal_breath_frontend/lib/utils/global.colors.dart
@@ -4,4 +4,5 @@ class GlobalColors {
   static HexColor mainColor = HexColor('#000252');
   static HexColor bgColor = HexColor('#E3EEFF');
   static HexColor textColor = HexColor('#000252');
+  static HexColor errColor = HexColor('#FC0023');
 }
