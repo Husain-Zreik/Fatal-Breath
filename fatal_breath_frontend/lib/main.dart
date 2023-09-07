4d70aa95a730166da594cbf18298212065ffe522
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-08 02:39:00 +0300

    Frontend <> edited the main file

--- fatal_breath_frontend/lib/main.dart
+++ fatal_breath_frontend/lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:fatal_breath_frontend/providers/auth.provider.dart';
+import 'package:fatal_breath_frontend/providers/user.provider.dart';
+import 'package:fatal_breath_frontend/screens/login.screen.dart';
+import 'package:fatal_breath_frontend/screens/signup.screen.dart';
 import 'package:fatal_breath_frontend/screens/splash.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        // ChangeNotifierProvider<Auth>(
+        //   create: (context) => Auth(),
+        // ),
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+        ChangeNotifierProvider<User>(
+          create: (context) => User(),
+        ),
+      ],
+      child: const MyApp(), // Your MaterialApp widget
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +31,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      initialRoute: '/splash',
+      getPages: [
+        GetPage(name: '/splash', page: () => const SplashScreen()),
+        GetPage(name: '/login', page: () => const LoginScreen()),
+        GetPage(name: '/signup', page: () => const SignUpScreen()),
+      ],
     );
   }
 }
