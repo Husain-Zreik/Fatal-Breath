caf5254fed4b63a3d8ab8e3ce87482d549520518
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-07 23:52:15 +0300

    Frontend <> created local storage config file

---
+++ fatal_breath_frontend/lib/config/local.storage.config.dart
@@ -0,0 +1,49 @@
+import "package:fatal_breath_frontend/enums/local.types.dart";
+import "package:shared_preferences/shared_preferences.dart";
+
+Future<void> localSave({
+  required LocalTypes type,
+  required String key,
+  required dynamic value,
+}) async {
+  SharedPreferences.setMockInitialValues({});
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  if (type == LocalTypes.Int) {
+    await prefs.setInt(key, value);
+  } else if (type == LocalTypes.Double) {
+    await prefs.setDouble(key, value);
+  } else if (type == LocalTypes.String) {
+    await prefs.setString(key, value);
+  } else if (type == LocalTypes.Bool) {
+    await prefs.setBool(key, value);
+  }
+}
+
+dynamic getLocal({
+  required LocalTypes type,
+  required String key,
+}) async {
+  // SharedPreferences.setMockInitialValues({});
+  SharedPreferences.setMockInitialValues({});
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  dynamic result;
+
+  if (type == LocalTypes.Int) {
+    result = prefs.getInt(key);
+  }
+  if (type == LocalTypes.Double) {
+    result = prefs.getDouble(key);
+  }
+  if (type == LocalTypes.String) {
+    result = prefs.getString(key);
+  }
+  if (type == LocalTypes.Bool) {
+    result = prefs.getBool(key);
+  }
+
+  return result;
+}
