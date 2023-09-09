84f25b24487b419fa94bb6d6410faf0dcb03fb2a
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-10 01:15:59 +0300

    Frontend <> created breathy screen

---
+++ fatal_breath_frontend/lib/screens/ai.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiScreen extends StatefulWidget {
+  const AiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AiScreen> createState() => _AiScreenState();
+}
+
+class _AiScreenState extends State<AiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
