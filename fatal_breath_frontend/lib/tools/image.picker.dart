e6a330d4d93ab03cd43b8a7fc6d70273b9ad293c
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-10 20:31:54 +0300

    Frontend <> created the image picker function

---
+++ fatal_breath_frontend/lib/tools/image.picker.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
+Future imagePicker() async {
+  try {
+    final XFile? inputImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (inputImage == null) return;
+
+    Uint8List imageBytes = await inputImage.readAsBytes();
+    String encoded = base64Url.encode(imageBytes);
+    Uint8List decoded = base64.decode(encoded);
+
+    Map imageInfo = {
+      "inputImage": inputImage,
+      "imageBytes": imageBytes,
+      "encoded": encoded,
+      "decoded": decoded,
+    };
+
+    return imageInfo;
+  } catch (e) {
+    throw HttpException(e.toString());
+  }
+}
