cbaf612fefcaf2eb6a691d089653dddde81a60af
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-12 01:19:36 +0300

    Frontend <> created the update profile function

--- fatal_breath_frontend/lib/providers/user.provider.dart
+++ fatal_breath_frontend/lib/providers/user.provider.dart
@@ -1,12 +1,16 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 
 import 'package:fatal_breath_frontend/config/remote.config.dart';
+import 'package:fatal_breath_frontend/enums/request.methods.dart';
 import 'package:flutter/cupertino.dart';
 
 class User with ChangeNotifier {
   String? name;
   String? username;
   String? email;
+  String? image;
   String? userType;
 
   String? get getName {
@@ -25,6 +29,16 @@ class User with ChangeNotifier {
     return userType;
   }
 
+  String? get getImage {
+    if (image != null) {
+      debugPrint("$baseUrl/storage/$image");
+      return "$baseUrl/storage/$image";
+    }
+
+    return "null";
+    // return null;
+  }
+
   //Get user data
   Future getUser(context) async {
     try {
@@ -33,6 +47,7 @@ class User with ChangeNotifier {
       name = response['user']["name"];
       username = response['user']["username"];
       email = response['user']["email"];
+      image = response['user']["profile_image"];
 
       if (response['user']["role"] == 1) {
         userType = "Admin";
@@ -46,5 +61,35 @@ class User with ChangeNotifier {
     }
   }
 
-  Future updateProfile() async {}
+  Future updateProfile(
+    name1,
+    username1,
+    email1,
+    profileImage1,
+    BuildContext context,
+  ) async {
+    try {
+      final body = {
+        'name': name1,
+        'username': username1,
+        'email': email1,
+        'profile_image': profileImage1,
+      };
+
+      final response = await sendRequest(
+          route: "/api/user/info/update",
+          method: RequestMethods.POST,
+          load: body);
+      print(response);
+
+      name = response['user']["name"];
+      username = response['user']["username"];
+      email = response['user']["email"];
+      image = response['user']["profile_image"];
+
+      notifyListeners();
+    } catch (error) {
+      throw HttpException('$error');
+    }
+  }
 }
