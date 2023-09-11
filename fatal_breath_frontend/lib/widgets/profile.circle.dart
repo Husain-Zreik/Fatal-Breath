654d541603eaf76d844f4f48fc65b554e8d11e4d
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-12 01:20:35 +0300

    Frontend <> added a decoded and url inputs for profile img

--- fatal_breath_frontend/lib/widgets/profile.circle.dart
+++ fatal_breath_frontend/lib/widgets/profile.circle.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 
 class ProfileCircle extends StatefulWidget {
-  const ProfileCircle({Key? key, required this.size, this.image})
+  const ProfileCircle(
+      {Key? key, required this.size, this.image, this.decoded, this.imageLink})
       : super(key: key);
 
   final double size;
+  final String? imageLink;
   final File? image;
+  final Uint8List? decoded;
 
   @override
   State<ProfileCircle> createState() => _ProfileCircleState();
@@ -25,19 +29,33 @@ class _ProfileCircleState extends State<ProfileCircle> {
             shape: BoxShape.circle,
             color: Colors.white,
           ),
-          child: widget.image != null
-              ? Image.file(
-                  widget.image!,
+          child: widget.decoded != null
+              ? Image.memory(
+                  widget.decoded!,
                   fit: BoxFit.cover,
                   width: widget.size,
                   height: widget.size,
                 )
-              : Image.asset(
-                  "assets/images/account_circle.png",
-                  fit: BoxFit.cover,
-                  width: widget.size,
-                  height: widget.size,
-                )),
+              : widget.image != null
+                  ? Image.file(
+                      widget.image!,
+                      fit: BoxFit.cover,
+                      width: widget.size,
+                      height: widget.size,
+                    )
+                  : widget.imageLink != null
+                      ? Image.network(
+                          widget.imageLink!,
+                          fit: BoxFit.cover,
+                          width: widget.size,
+                          height: widget.size,
+                        )
+                      : Image.asset(
+                          "assets/images/account_circle.png",
+                          fit: BoxFit.cover,
+                          width: widget.size,
+                          height: widget.size,
+                        )),
     );
   }
 }
