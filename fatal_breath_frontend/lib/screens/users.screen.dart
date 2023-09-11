3e2ec642fa370df06ef9a84c8bed4b25a0799922
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-12 01:22:24 +0300

    Frontend <> removed the container color

--- fatal_breath_frontend/lib/screens/users.screen.dart
+++ fatal_breath_frontend/lib/screens/users.screen.dart
@@ -1,4 +1,3 @@
-import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:flutter/material.dart';
 
 class UsersScreen extends StatefulWidget {
@@ -11,8 +10,6 @@ class UsersScreen extends StatefulWidget {
 class _UsersScreenState extends State<UsersScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: GlobalColors.mainColor,
-    );
+    return Container();
   }
 }
