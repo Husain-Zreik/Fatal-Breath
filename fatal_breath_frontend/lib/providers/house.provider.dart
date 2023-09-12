f5e9e4f237dc5747e909d250917e924eb2a243a2
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 00:00:04 +0300

    Frontend <> created the get houses method

--- fatal_breath_frontend/lib/providers/house.provider.dart
+++ fatal_breath_frontend/lib/providers/house.provider.dart
@@ -6,9 +6,14 @@ import 'package:flutter/material.dart';
 
 class HouseProvider with ChangeNotifier {
   String? houseName;
+  List? adminHouses;
 
   String? get getHouseName {
     return houseName;
+  }
+
+  List? get getAdminHouses {
+    return adminHouses;
   }
 
   Future createHouse(name, country, city, context) async {
@@ -22,8 +27,39 @@ class HouseProvider with ChangeNotifier {
           route: "/api/user/admin/add-house",
           method: RequestMethods.POST,
           load: body);
-
+      print(response);
       houseName = response['house']["name"];
+
+      notifyListeners();
+    } catch (e) {
+      print(e);
+      throw HttpException('$e');
+    }
+  }
+
+  Future getHouses() async {
+    try {
+      final response = await sendRequest(
+        route: "/api/user/admin/get-houses",
+        method: RequestMethods.GET,
+      );
+
+      print(response);
+
+      final List<dynamic> houseList = response['houses'];
+      List<Map<String, dynamic>> houses = [];
+
+      for (var houseData in houseList) {
+        Map<String, dynamic> house = {
+          'name': houseData['name'],
+          'city': houseData['city'],
+          'country': houseData['country'],
+        };
+        houses.add(house);
+      }
+      print(houses);
+
+      adminHouses = houses;
 
       notifyListeners();
     } catch (e) {
