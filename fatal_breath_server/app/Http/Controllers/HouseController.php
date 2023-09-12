53d66226b98f1e2abb4410878e0adcc5d477aa18
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-12 21:09:45 +0300

    Server <> created the createHouse api

---
+++ fatal_breath_server/app/Http/Controllers/HouseController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\House;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class HouseController extends Controller
+{
+    public function createHouse(Request $request)
+    {
+        try {
+            $user = Auth::user();
+
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'city' => 'required|string|max:255',
+                'country' => 'required|string|max:255',
+            ]);
+
+            $house = new House();
+            $house->owner_id = $user->id;
+            $house->name = $request->name;
+            $house->city = $request->city;
+            $house->country = $request->country;
+            $house->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'House created successfully',
+                'house' => $house,
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+    }
+}
