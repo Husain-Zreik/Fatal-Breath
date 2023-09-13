e027365dcd90bb95aa36a67313214932583cd057
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 21:35:09 +0300

    Server <> created the createRoom api

---
+++ fatal_breath_server/app/Http/Controllers/RoomController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class RoomController extends Controller
+{
+    public function createRoom(Request $request)
+    {
+        try {
+            $request->validate([
+                'house_id' => 'required|exists:houses,id',
+                'type' => 'required|in:Kitchen,Bedroom,Livingroom,Bathroom',
+                'name' => 'required|string|max:255',
+            ]);
+
+            $room = new Room();
+            $room->house_id = $request->house_id;
+            $room->name = $request->name;
+            $room->type = $request->type;
+            $room->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Room created successfully',
+                'room' => $room,
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+    }
+}
