f7f158969278f307f9b5e6f3913438763dbab3f8
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 17:49:09 +0300

    Frontend <> edited the house model

--- fatal_breath_frontend/lib/models/house.model.dart
+++ fatal_breath_frontend/lib/models/house.model.dart
@@ -1,15 +1,42 @@
 import 'package:fatal_breath_frontend/models/room.model.dart';
+import 'package:fatal_breath_frontend/models/user.model.dart';
 
-class HouseWithRooms {
+class House {
+  final int id;
   final String name;
+  final int ownerId;
   final String city;
   final String country;
-  final List<Room> rooms;
+  final List<Room>? rooms;
+  final List<User>? members;
 
-  HouseWithRooms({
+  House({
+    required this.id,
     required this.name,
+    required this.ownerId,
     required this.city,
     required this.country,
-    required this.rooms,
+    this.rooms,
+    this.members,
   });
+
+  factory House.fromJson(Map<String, dynamic> json) {
+    List<Room> rooms = (json['rooms'] as List<dynamic>)
+        .map((roomJson) => Room.fromJson(roomJson))
+        .toList();
+
+    List<User> members = (json['members'] as List<dynamic>)
+        .map((memberJson) => User.fromJson(memberJson))
+        .toList();
+
+    return House(
+      id: json['id'],
+      name: json['name'],
+      ownerId: json['owner_id'],
+      city: json['city'],
+      country: json['country'],
+      rooms: rooms,
+      members: members,
+    );
+  }
 }
