8dcec2d253f58d5dcd71b7e3ea98ffce88ee3906
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 14:11:42 +0300

    Frontend <> created the add room screen

---
+++ fatal_breath_frontend/lib/screens/home/add.room.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddRoomScreen extends StatefulWidget {
+  const AddRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddRoomScreen> createState() => _AddRoomScreenState();
+}
+
+class _AddRoomScreenState extends State<AddRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
