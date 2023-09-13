865761fcb349ebc8fcf67e3efc4376cb8735286d
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 23:34:34 +0300

    Frontend <> styled the rooms display

--- fatal_breath_frontend/lib/screens/home.screen.dart
+++ fatal_breath_frontend/lib/screens/home.screen.dart
@@ -93,15 +93,58 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   for (final house in houses!)
                     house.rooms != null && house.rooms!.isNotEmpty
-                        ? ListView.builder(
-                            itemCount: house.rooms!.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              final room = house.rooms![index];
-                              return ListTile(
-                                title: Text(room.name),
-                              );
-                            },
-                          )
+                        ?
+                        // ListView.builder(
+                        //     itemCount: house.rooms!.length,
+                        //     itemBuilder: (BuildContext context, int index) {
+                        //       final room = house.rooms![index];
+                        //       return ListTile(
+                        //         title: Text(room.name),
+                        //       );
+                        //     },
+                        //   )
+                        SingleChildScrollView(
+                            child: Column(children: [
+                            for (final room in house.rooms!)
+                              Container(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 30),
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/images/' + room.type + '.png',
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(10),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(room.name),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                          ]))
                         : HouseEmptyStateScreen(
                             houseId: house.id,
                           ),
