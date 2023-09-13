376ca395c92da71c015ab115090c22132a31dbf4
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 21:39:05 +0300

    Frontend <> created the createRoom method

---
+++ fatal_breath_frontend/lib/providers/room.provider.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
+import 'package:fatal_breath_frontend/config/remote.config.dart';
+import 'package:fatal_breath_frontend/enums/request.methods.dart';
+import 'package:flutter/material.dart';
+
+class HouseProvider with ChangeNotifier {
+  Future createHouse(name, type, house, context) async {
+    try {
+      final body = {
+        'name': name,
+        'type': type,
+        'house_id': house,
+      };
+      final response = await sendRequest(
+          route: "/api/user/admin/add-room",
+          method: RequestMethods.POST,
+          load: body);
+
+      print(response);
+
+      notifyListeners();
+    } catch (e) {
+      throw HttpException('$e');
+    }
+  }
+}
