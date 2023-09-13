9f55cc442a1604694699c0a02b4f8feabab715f6
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 14:36:35 +0300

    Frontend <> edited the room emptystate

--- fatal_breath_frontend/lib/screens/empty/room.empty.state.screen.dart
+++ fatal_breath_frontend/lib/screens/empty/room.empty.state.screen.dart
@@ -1,10 +1,37 @@
+import 'package:fatal_breath_frontend/screens/home/add.room.screen.dart';
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
+import 'package:fatal_breath_frontend/utils/text.note.dart';
+import 'package:fatal_breath_frontend/utils/text.title.dart';
+import 'package:fatal_breath_frontend/widgets/button.global.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class RoomEmptyStateScreen extends StatelessWidget {
   const RoomEmptyStateScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextTitle(text: "Your Rooms"),
+            const SizedBox(height: 10),
+            const TextNote(text: "Get started by adding your rooms"),
+            const SizedBox(height: 30),
+            ButtonGlobal(
+              bgColor: GlobalColors.mainColor,
+              textColor: Colors.white,
+              icon: Icons.add,
+              onBtnPressed: () {
+                Get.to(() => const AddRoomScreen());
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
