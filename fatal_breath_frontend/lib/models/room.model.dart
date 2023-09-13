e8b82f3451291eda2e4c26eeb14e4ab4ab6be3d3
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 17:49:21 +0300

    Frontend <> edited the room model

--- fatal_breath_frontend/lib/models/room.model.dart
+++ fatal_breath_frontend/lib/models/room.model.dart
@@ -1,11 +1,25 @@
 class Room {
+  final int id;
+  final int houseId;
   final String type;
   final String name;
   final String size;
 
   Room({
+    required this.id,
+    required this.houseId,
     required this.type,
     required this.name,
     required this.size,
   });
+
+  factory Room.fromJson(Map<String, dynamic> json) {
+    return Room(
+      id: json['id'],
+      houseId: json['house_id'],
+      type: json['type'],
+      name: json['name'],
+      size: json['size'],
+    );
+  }
 }
