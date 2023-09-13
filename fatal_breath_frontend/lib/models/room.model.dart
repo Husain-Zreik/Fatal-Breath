5e63d3e4a1291e8d4fd8a4d6eb6617952f7db079
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 22:57:05 +0300

    Frontend <> removed the size attribute

--- fatal_breath_frontend/lib/models/room.model.dart
+++ fatal_breath_frontend/lib/models/room.model.dart
@@ -3,14 +3,12 @@ class Room {
   final int houseId;
   final String type;
   final String name;
-  final String size;
 
   Room({
     required this.id,
     required this.houseId,
     required this.type,
     required this.name,
-    required this.size,
   });
 
   factory Room.fromJson(Map<String, dynamic> json) {
@@ -19,7 +17,6 @@ class Room {
       houseId: json['house_id'],
       type: json['type'],
       name: json['name'],
-      size: json['size'],
     );
   }
 }
