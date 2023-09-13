5b49c4c8b5dde24603f36c31a36f062262cbe9e9
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-13 23:00:41 +0300

    Frontend <> created the add room screen

--- fatal_breath_frontend/lib/screens/home/add.room.screen.dart
+++ fatal_breath_frontend/lib/screens/home/add.room.screen.dart
@@ -1,15 +1,140 @@
+import 'dart:io';
+
+import 'package:fatal_breath_frontend/enums/room.types.dart';
+import 'package:fatal_breath_frontend/providers/house.provider.dart';
+import 'package:fatal_breath_frontend/providers/room.provider.dart';
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
+import 'package:fatal_breath_frontend/utils/text.error.dart';
+import 'package:fatal_breath_frontend/widgets/button.global.dart';
+import 'package:fatal_breath_frontend/widgets/drop.down.dart';
+import 'package:fatal_breath_frontend/widgets/secondary.appbar.dart';
+import 'package:fatal_breath_frontend/widgets/text.form.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class AddRoomScreen extends StatefulWidget {
-  const AddRoomScreen({Key? key}) : super(key: key);
+  const AddRoomScreen({Key? key, required this.houseId}) : super(key: key);
+
+  final int houseId;
 
   @override
   State<AddRoomScreen> createState() => _AddRoomScreenState();
 }
 
 class _AddRoomScreenState extends State<AddRoomScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController oldpasswordController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
+  String? _selectedType;
+  List<String> roomTypes = RoomTypes.roomTypes;
+
+  String err = "";
+  bool validated() {
+    return _form.currentState!.validate();
+  }
+
+  Future createPressed(name, type, house, context) async {
+    try {
+      setState(() {
+        err = "";
+      });
+
+      if (!validated()) {
+        return err = "Fill the inputs correctly";
+      }
+
+      await Provider.of<RoomProvider>(context, listen: false)
+          .createRoom(name, type, house, context);
+
+      await Provider.of<HouseProvider>(context, listen: false).getAdminHouses();
+
+      Get.back();
+    } on HttpException catch (error) {
+      setState(() {
+        err = error.message;
+      });
+    }
+  }
+
+  inputvalidator(value) {
+    if (value!.isEmpty) {
+      return "Please re-enter your password";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SecondaryAppBar(title: "Add Room"),
+      ),
+      backgroundColor: GlobalColors.bgColor,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+            child: Form(
+              key: _form,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextForm(
+                    textInputType: TextInputType.text,
+                    controller: nameController,
+                    label: 'Room Name',
+                    hintText: 'Enter the room name',
+                    isPass: false,
+                    validator: inputvalidator,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomDropdown<String>(
+                    label: 'Select Room Type',
+                    value: _selectedType,
+                    items: roomTypes.map((String type) {
+                      return DropdownMenuItem<String>(
+                        value: type,
+                        child: Text(type),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedType = newValue;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ButtonGlobal(
+                      text: 'Create',
+                      bgColor: GlobalColors.mainColor,
+                      textColor: Colors.white,
+                      onBtnPressed: () {
+                        createPressed(nameController.text, _selectedType,
+                            widget.houseId, context);
+                      }),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextError(text: err),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
