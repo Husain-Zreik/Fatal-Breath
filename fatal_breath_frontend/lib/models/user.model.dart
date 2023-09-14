a93af0e92debd842e354244a1daba5e24098ff24
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-14 21:15:23 +0300

    Frontend <> added the name to the user model

--- fatal_breath_frontend/lib/models/user.model.dart
+++ fatal_breath_frontend/lib/models/user.model.dart
@@ -1,11 +1,13 @@
 class User {
   final int id;
   final String username;
+  final String name;
   final String email;
 
   User({
     required this.id,
     required this.username,
+    required this.name,
     required this.email,
   });
 
@@ -13,6 +15,7 @@ class User {
     return User(
       id: json['id'],
       username: json['username'],
+      name: json['name'],
       email: json['email'],
     );
   }
