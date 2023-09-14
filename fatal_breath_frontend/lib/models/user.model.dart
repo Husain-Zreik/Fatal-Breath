517f70e5cfef4d314bc3bb1ff063044edb734daa
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-14 21:44:10 +0300

    Frontend <> added the role attribute to the model

--- fatal_breath_frontend/lib/models/user.model.dart
+++ fatal_breath_frontend/lib/models/user.model.dart
@@ -1,22 +1,28 @@
 class User {
   final int id;
+  final int role;
   final String username;
   final String name;
   final String email;
+  final String? profileImage;
 
   User({
     required this.id,
+    required this.role,
     required this.username,
     required this.name,
     required this.email,
+    this.profileImage,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
+      role: json['role'],
       username: json['username'],
       name: json['name'],
       email: json['email'],
+      profileImage: json['profile_image'],
     );
   }
 }
