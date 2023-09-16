a3daba5fa3dff0ab7b5413df4fb4c30e81fa417c
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-16 18:16:56 +0300

    Frontend <> added the isInvited attribute

--- fatal_breath_frontend/lib/models/user.model.dart
+++ fatal_breath_frontend/lib/models/user.model.dart
@@ -5,6 +5,7 @@ class User {
   final String name;
   final String email;
   final String? profileImage;
+  final bool? isInvited;
 
   User({
     required this.id,
@@ -13,6 +14,7 @@ class User {
     required this.name,
     required this.email,
     this.profileImage,
+    this.isInvited,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
@@ -23,6 +25,7 @@ class User {
       name: json['name'],
       email: json['email'],
       profileImage: json['profile_image'],
+      isInvited: json['isInvited'],
     );
   }
 }
