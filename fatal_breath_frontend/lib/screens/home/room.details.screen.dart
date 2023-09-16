1556b12ade4746e6b10d7dbe790384045ee9a84a
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-16 19:28:03 +0300

    Frontend <> converted the room details screen to stateless

--- fatal_breath_frontend/lib/screens/home/room.details.screen.dart
+++ fatal_breath_frontend/lib/screens/home/room.details.screen.dart
@@ -1,21 +1,19 @@
+import 'package:fatal_breath_frontend/models/room.model.dart';
 import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:fatal_breath_frontend/widgets/secondary.appbar.dart';
 import 'package:flutter/material.dart';
 
-class RoomDetailsScreen extends StatefulWidget {
-  const RoomDetailsScreen({Key? key}) : super(key: key);
+class RoomDetailsScreen extends StatelessWidget {
+  const RoomDetailsScreen({super.key, required this.room});
 
-  @override
-  State<RoomDetailsScreen> createState() => _RoomDetailsScreenState();
-}
+  final Room room;
 
-class _RoomDetailsScreenState extends State<RoomDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: SecondaryAppBar(title: "Add Room"),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: SecondaryAppBar(title: room.name),
       ),
       backgroundColor: GlobalColors.bgColor,
     );
