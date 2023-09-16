362c48b16ac08625d9996d45678c6550fccfda8e
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-16 18:44:47 +0300

    Frontend <> created the room details screen

---
+++ fatal_breath_frontend/lib/screens/home/room.details.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoomDetailsScreen extends StatefulWidget {
+  const RoomDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RoomDetailsScreen> createState() => _RoomDetailsScreenState();
+}
+
+class _RoomDetailsScreenState extends State<RoomDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
