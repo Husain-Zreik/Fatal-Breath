bedc0f20fea2aa0dec41b6bcae723c2f9ed62cc3
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-16 18:47:38 +0300

    Frontend <> added the app bar for the room details screen

--- fatal_breath_frontend/lib/screens/home/room.details.screen.dart
+++ fatal_breath_frontend/lib/screens/home/room.details.screen.dart
@@ -1,3 +1,5 @@
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
+import 'package:fatal_breath_frontend/widgets/secondary.appbar.dart';
 import 'package:flutter/material.dart';
 
 class RoomDetailsScreen extends StatefulWidget {
@@ -10,6 +12,12 @@ class RoomDetailsScreen extends StatefulWidget {
 class _RoomDetailsScreenState extends State<RoomDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SecondaryAppBar(title: "Add Room"),
+      ),
+      backgroundColor: GlobalColors.bgColor,
+    );
   }
 }
