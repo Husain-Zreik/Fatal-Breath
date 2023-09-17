d2c9e80bb33fb844b68d01bbff9d9545e75ffad3
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 16:04:22 +0300

    Frontend <> created fetch weather method

--- fatal_breath_frontend/lib/providers/room.provider.dart
+++ fatal_breath_frontend/lib/providers/room.provider.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: avoid_print
 
-import 'dart:convert';
 import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+import 'package:dio/dio.dart';
 import 'package:fatal_breath_frontend/config/remote.config.dart';
 import 'package:fatal_breath_frontend/enums/request.methods.dart';
 import 'package:flutter/material.dart';
@@ -28,27 +30,65 @@ class RoomProvider with ChangeNotifier {
     }
   }
 
-  Future fetchWeather(country, city, context) async {
-    try {
-      const apiKey = '232dfd47c590be56ac3d87bf82aab944';
-      const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  Future fetchWeather(city, country) async {
+    const apiKey = '232dfd47c590be56ac3d87bf82aab944';
+    // const apiKey = '914536411a15b4683380209ccf43fb0f';
+    final apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city,$country&units=metric&appid=$apiKey';
+    final dio = Dio();
 
-      final response = await sendRequest(
-        route: "'$apiUrl?q=$city,$country&units=metric&appid=$apiKey'",
-        method: RequestMethods.GET,
+    try {
+      final response = await dio.get(
+        apiUrl,
       );
 
-      final data = json.decode(response);
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> weatherData = {
+          'temp': response.data['main']['temp'],
+          'humidity': response.data['main']['humidity'],
+          'wind': response.data['wind']['speed'],
+        };
 
-      final body = {
-        'temp': data.main.temp,
-        'humidity': data.main.humidity,
-        'wind': data.wind.speed,
-      };
-
-      return body;
+        return weatherData;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
     } catch (e) {
       throw HttpException('$e');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchWeather1(city, country, context) async {
+    const apiKey = '232dfd47c590be56ac3d87bf82aab944';
+    final apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city,$country&units=metric&appid=$apiKey';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl), headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+      });
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> weatherData = {
+          'temp': json.decode(response.body)['main']['temp'],
+          'humidity': json.decode(response.body)['main']['humidity'],
+          'wind': json.decode(response.body)['wind']['speed'],
+        };
+
+        return weatherData;
+      } else if (response.statusCode == 404) {
+        throw Exception('Not found');
+      } else if (response.statusCode == 500) {
+        throw Exception('Server not responding');
+      } else {
+        throw Exception('Some other error');
+      }
+    } catch (e) {
+      throw Exception('$e');
     }
   }
 }
