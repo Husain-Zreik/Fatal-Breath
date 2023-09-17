5ab0d20591bd0773c6e7b3270b76f8160416f0db
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 21:45:44 +0300

    Frontend <> edited the user empty state screen

--- fatal_breath_frontend/lib/screens/empty/user.empty.state.screen.dart
+++ fatal_breath_frontend/lib/screens/empty/user.empty.state.screen.dart
@@ -1,10 +1,23 @@
+import 'package:fatal_breath_frontend/utils/text.note.dart';
 import 'package:flutter/material.dart';
 
 class UserEmptyStateScreen extends StatelessWidget {
-  const UserEmptyStateScreen({Key? key}) : super(key: key);
+  const UserEmptyStateScreen({Key? key, required this.text}) : super(key: key);
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextNote(text: text),
+          ],
+        ),
+      ),
+    );
   }
 }
