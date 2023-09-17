aa9780592290a2ac2c5224e98867eebb9bd98bde
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 19:13:07 +0300

    Frontend <> added an appbar

--- fatal_breath_frontend/lib/screens/chats.screen.dart
+++ fatal_breath_frontend/lib/screens/chats.screen.dart
@@ -1,10 +1,13 @@
 import 'package:fatal_breath_frontend/providers/house.provider.dart';
+import 'package:fatal_breath_frontend/providers/user.provider.dart';
 import 'package:fatal_breath_frontend/screens/empty/home.empty.state.screen.dart';
 import 'package:fatal_breath_frontend/screens/messages.screen.dart';
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:fatal_breath_frontend/utils/text.note.dart';
 import 'package:fatal_breath_frontend/widgets/contact.box.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class ChatsScreen extends StatefulWidget {
@@ -17,6 +20,7 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   List? houses;
   List? members;
+  String? image;
 
   @override
   void initState() {
@@ -28,6 +32,8 @@ class _ChatsScreenState extends State<ChatsScreen> {
   Widget build(BuildContext context) {
     houses = Provider.of<HouseProvider>(context).getHouses ?? [];
     members = Provider.of<HouseProvider>(context).getMembers ?? [];
+    image = Provider.of<UserProvider>(context, listen: false).getImage;
+
     return houses!.isEmpty
         ? const HomeEmptyStateScreen()
         : members!.isEmpty
@@ -38,22 +44,74 @@ class _ChatsScreenState extends State<ChatsScreen> {
                       text: "No members in your houses go and invite some.")
                 ],
               )
-            : SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 20, left: 5, right: 5, bottom: 30),
-                    child: Column(
-                      children: [
-                        for (final user in members!)
-                          InkWell(
-                            onTap: () {
-                              Get.to(() => MessagesScreen(user: user));
-                            },
-                            child: ContactBox(
-                              user: user,
-                            ),
+            : Scaffold(
+                backgroundColor: GlobalColors.bgColor,
+                appBar: AppBar(
+                  backgroundColor: GlobalColors.bgColor,
+                  elevation: 0,
+                  centerTitle: true,
+                  leadingWidth: 65,
+                  toolbarHeight: 80,
+                  leading: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Image.asset(
+                      'assets/images/light_icon.png',
+                    ),
+                  ),
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 13),
+                    child: Text(
+                      "Chats",
+                      style: GoogleFonts.poppins(
+                        color: GlobalColors.mainColor,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 15, bottom: 8, right: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Container(
+                          width: 55,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
                           ),
-                      ],
-                    )));
+                          child: image == null
+                              ? Image.asset(
+                                  'assets/images/account_circle.png',
+                                  fit: BoxFit.cover,
+                                )
+                              : Image.network(
+                                  image!,
+                                  fit: BoxFit.cover,
+                                ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                body: SingleChildScrollView(
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, left: 5, right: 5, bottom: 30),
+                        child: Column(
+                          children: [
+                            for (final user in members!)
+                              InkWell(
+                                onTap: () {
+                                  Get.to(() => MessagesScreen(user: user));
+                                },
+                                child: ContactBox(
+                                  user: user,
+                                ),
+                              ),
+                          ],
+                        ))),
+              );
   }
 }
