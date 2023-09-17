87d7796d2138d3b6eccdce42f4c4ec5b5cc206f0
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 18:28:35 +0300

    Frontend <> created the body of the screen

--- fatal_breath_frontend/lib/screens/chats.screen.dart
+++ fatal_breath_frontend/lib/screens/chats.screen.dart
@@ -1,6 +1,7 @@
 import 'package:fatal_breath_frontend/providers/house.provider.dart';
 import 'package:fatal_breath_frontend/screens/empty/home.empty.state.screen.dart';
 import 'package:fatal_breath_frontend/utils/text.note.dart';
+import 'package:fatal_breath_frontend/widgets/contact.box.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -35,6 +36,16 @@ class _ChatsScreenState extends State<ChatsScreen> {
                       text: "No members in your houses go and invite some.")
                 ],
               )
-            : Container();
+            : SingleChildScrollView(
+                child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        for (final user in members!)
+                          ContactBox(
+                            user: user,
+                          ),
+                      ],
+                    )));
   }
 }
