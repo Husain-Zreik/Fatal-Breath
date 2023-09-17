16b0cb85804a7bcdd5242d9da4b529bafaf0ad92
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 23:28:43 +0300

    Frontend <> created find house screen

---
+++ fatal_breath_frontend/lib/screens/find.house.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FindHouseScreen extends StatefulWidget {
+  const FindHouseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FindHouseScreen> createState() => _FindHouseScreenState();
+}
+
+class _FindHouseScreenState extends State<FindHouseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
