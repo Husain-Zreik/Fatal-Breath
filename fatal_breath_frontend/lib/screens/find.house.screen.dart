1ce72d4d8fa9a554212195bb97fefaaebfff5cea
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 23:41:20 +0300

    Frontend <> created the app bar

--- fatal_breath_frontend/lib/screens/find.house.screen.dart
+++ fatal_breath_frontend/lib/screens/find.house.screen.dart
@@ -1,6 +1,8 @@
 import 'package:fatal_breath_frontend/providers/house.provider.dart';
 import 'package:fatal_breath_frontend/providers/user.provider.dart';
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class FindHouseScreen extends StatefulWidget {
@@ -24,6 +26,55 @@ class _FindHouseScreenState extends State<FindHouseScreen> {
   Widget build(BuildContext context) {
     houses = Provider.of<HouseProvider>(context).getHouses ?? [];
     image = Provider.of<UserProvider>(context, listen: false).getImage;
-    return Container();
+    return Scaffold(
+        backgroundColor: GlobalColors.bgColor,
+        appBar: AppBar(
+          backgroundColor: GlobalColors.bgColor,
+          elevation: 0,
+          centerTitle: true,
+          leadingWidth: 65,
+          toolbarHeight: 80,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Image.asset(
+              'assets/images/light_icon.png',
+            ),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(top: 13),
+            child: Text(
+              "Find House",
+              style: GoogleFonts.poppins(
+                color: GlobalColors.mainColor,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 17, bottom: 10, right: 10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Container(
+                  width: 53,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: image == null
+                      ? Image.asset(
+                          'assets/images/account_circle.png',
+                          fit: BoxFit.cover,
+                        )
+                      : Image.network(
+                          image!,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
