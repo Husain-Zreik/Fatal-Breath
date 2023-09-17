c34bf64ebaef7c87bdb720da8b156f676a4e76ac
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 18:56:03 +0300

    Frontend <> created the messages screen structure

--- fatal_breath_frontend/lib/screens/messages.screen.dart
+++ fatal_breath_frontend/lib/screens/messages.screen.dart
@@ -1,25 +1,29 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:fatal_breath_frontend/models/user.model.dart';
 import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:fatal_breath_frontend/widgets/secondary.appbar.dart';
 import 'package:flutter/material.dart';
 
-class EditProfileScreen extends StatefulWidget {
-  const EditProfileScreen({Key? key}) : super(key: key);
+class MessagesScreen extends StatefulWidget {
+  const MessagesScreen({super.key, required this.user});
+
+  final User user;
 
   @override
-  State<EditProfileScreen> createState() => _EditProfileScreenState();
+  State<MessagesScreen> createState() => _MessagesScreenState();
 }
 
-class _EditProfileScreenState extends State<EditProfileScreen> {
+class _MessagesScreenState extends State<MessagesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
+        preferredSize: const Size.fromHeight(80),
         child: SecondaryAppBar(
-          title: "Edit Profile",
+          title: widget.user.name,
           isProfile: true,
+          imageLink: widget.user.profileImage == null
+              ? null
+              : 'http://192.168.1.5:8000/storage/profile_images/${widget.user.id}.png',
         ),
       ),
       backgroundColor: GlobalColors.bgColor,
