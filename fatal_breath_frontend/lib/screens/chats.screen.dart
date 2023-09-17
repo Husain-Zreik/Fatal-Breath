4244a5c9aadcc88921e59d97fed7ebf9adc5bdbf
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 23:07:02 +0300

    frontend <> edited the empty state of the chats screen

--- fatal_breath_frontend/lib/screens/chats.screen.dart
+++ fatal_breath_frontend/lib/screens/chats.screen.dart
@@ -35,133 +35,129 @@ class _ChatsScreenState extends State<ChatsScreen> {
     members = Provider.of<HouseProvider>(context).getMembers ?? [];
     image = Provider.of<UserProvider>(context, listen: false).getImage;
 
-    return houses!.isEmpty
-        ? const HomeEmptyStateScreen()
-        : members!.isEmpty
-            ? const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      backgroundColor: GlobalColors.bgColor,
+      appBar: AppBar(
+        backgroundColor: GlobalColors.bgColor,
+        elevation: 0,
+        centerTitle: true,
+        leadingWidth: 65,
+        toolbarHeight: 80,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset(
+            'assets/images/light_icon.png',
+          ),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 13),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  TextNote(
-                      text: "No members in your houses go and invite some.")
+                  Text(
+                    'FATAL',
+                    style: TextStyle(
+                      color: GlobalColors.textColor,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  Text(
+                    'CHAT',
+                    style: TextStyle(
+                      color: HexColor('#1424B9'),
+                      fontSize: 26,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ],
-              )
-            : Scaffold(
-                backgroundColor: GlobalColors.bgColor,
-                appBar: AppBar(
-                  backgroundColor: GlobalColors.bgColor,
-                  elevation: 0,
-                  centerTitle: true,
-                  leadingWidth: 65,
-                  toolbarHeight: 80,
-                  leading: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Image.asset(
-                      'assets/images/light_icon.png',
-                    ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 10,
+                    width: 10,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.green),
                   ),
-                  title: Padding(
-                    padding: const EdgeInsets.only(top: 13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              'FATAL',
-                              style: TextStyle(
-                                color: GlobalColors.textColor,
-                                fontSize: 26,
-                                fontWeight: FontWeight.w900,
-                              ),
-                            ),
-                            Text(
-                              'CHAT',
-                              style: TextStyle(
-                                color: HexColor('#1424B9'),
-                                fontSize: 26,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 10,
-                              width: 10,
-                              decoration: const BoxDecoration(
-                                  shape: BoxShape.circle, color: Colors.green),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text("Online",
-                                style: GoogleFonts.poppins(
-                                  color: GlobalColors.mainColor,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                          ],
-                        ),
-                      ],
-                    ),
+                  const SizedBox(
+                    width: 10,
                   ),
-                  // title: Padding(
-                  //   padding: const EdgeInsets.only(top: 13),
-                  //   child: Text(
-                  //     "Chats",
-                  //     style: GoogleFonts.poppins(
-                  //       color: GlobalColors.mainColor,
-                  //       fontSize: 24,
-                  //       fontWeight: FontWeight.w600,
-                  //     ),
-                  //   ),
-                  // ),
-                  actions: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 17, bottom: 10, right: 10),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Container(
-                          width: 53,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          child: image == null
-                              ? Image.asset(
-                                  'assets/images/account_circle.png',
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.network(
-                                  image!,
-                                  fit: BoxFit.cover,
-                                ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  Text("Online",
+                      style: GoogleFonts.poppins(
+                        color: GlobalColors.mainColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ),
+        // title: Padding(
+        //   padding: const EdgeInsets.only(top: 13),
+        //   child: Text(
+        //     "Chats",
+        //     style: GoogleFonts.poppins(
+        //       color: GlobalColors.mainColor,
+        //       fontSize: 24,
+        //       fontWeight: FontWeight.w600,
+        //     ),
+        //   ),
+        // ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(top: 17, bottom: 10, right: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Container(
+                width: 53,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
                 ),
-                body: SingleChildScrollView(
-                    child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 20, left: 5, right: 5, bottom: 30),
-                        child: Column(
-                          children: [
-                            for (final user in members!)
-                              InkWell(
-                                onTap: () {
-                                  Get.to(() => MessagesScreen(user: user));
-                                },
-                                child: ContactBox(
-                                  user: user,
-                                ),
+                child: image == null
+                    ? Image.asset(
+                        'assets/images/account_circle.png',
+                        fit: BoxFit.cover,
+                      )
+                    : Image.network(
+                        image!,
+                        fit: BoxFit.cover,
+                      ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: houses!.isEmpty
+          ? const HomeEmptyStateScreen()
+          : members!.isEmpty
+              ? const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [TextNote(text: "No members in your houses")],
+                )
+              : SingleChildScrollView(
+                  child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 20, left: 5, right: 5, bottom: 30),
+                      child: Column(
+                        children: [
+                          for (final user in members!)
+                            InkWell(
+                              onTap: () {
+                                Get.to(() => MessagesScreen(user: user));
+                              },
+                              child: ContactBox(
+                                user: user,
                               ),
-                          ],
-                        ))),
-              );
+                            ),
+                        ],
+                      ))),
+    );
   }
 }
