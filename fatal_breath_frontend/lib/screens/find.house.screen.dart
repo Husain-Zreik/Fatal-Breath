3896564fb03afe149b1070caf7020c1f0384d19e
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-17 23:32:16 +0300

    Frontend <> initialized the houses

--- fatal_breath_frontend/lib/screens/find.house.screen.dart
+++ fatal_breath_frontend/lib/screens/find.house.screen.dart
@@ -1,4 +1,7 @@
+import 'package:fatal_breath_frontend/providers/house.provider.dart';
+import 'package:fatal_breath_frontend/providers/user.provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FindHouseScreen extends StatefulWidget {
   const FindHouseScreen({Key? key}) : super(key: key);
@@ -8,8 +11,19 @@ class FindHouseScreen extends StatefulWidget {
 }
 
 class _FindHouseScreenState extends State<FindHouseScreen> {
+  String? image;
+  List? houses;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<HouseProvider>(context, listen: false).getUserHouses();
+  }
+
   @override
   Widget build(BuildContext context) {
+    houses = Provider.of<HouseProvider>(context).getHouses ?? [];
+    image = Provider.of<UserProvider>(context, listen: false).getImage;
     return Container();
   }
 }
