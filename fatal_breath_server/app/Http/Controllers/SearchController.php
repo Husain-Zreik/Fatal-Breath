5236b9a6392f06b144a2507a11534eb022810e14
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-19 16:16:57 +0300

    Server <> added the usersearch api

--- fatal_breath_server/app/Http/Controllers/SearchController.php
+++ fatal_breath_server/app/Http/Controllers/SearchController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MembershipRequest;
+use App\Models\User;
+use App\Models\UserHouse;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function searchUsers(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'house_id' => 'required|integer',
+        ]);
+
+        $username = $request->input('username');
+        $houseId = $request->input('house_id');
+
+        $adminUserIds = User::where('role', 1)->pluck('id')->toArray();
+
+        $houseMemberIds = UserHouse::where('house_id', $houseId)->pluck('user_id')->toArray();
+
+        $users = User::whereNotIn('id', $adminUserIds)
+            ->whereNotIn('id', $houseMemberIds)
+            ->where('username', 'LIKE', "%$username%")
+            ->get();
+
+        foreach ($users as $user) {
+            $invitationExists = MembershipRequest::where('house_id', $houseId)
+                ->where('user_id', $user->id)
+                ->where('type', 'Invitation')
+                ->where('status', 'Pending')
+                ->exists();
+            $user->isInvited = $invitationExists;
+        }
+
+        return response()->json(['users' => $users]);
+    }
 }
