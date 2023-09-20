2d03260077d7f7f32a54e9a5d152403641e1adbc
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-20 20:31:40 +0300

    Server <> created updateLevel api

--- fatal_breath_server/app/Http/Controllers/SensorController.php
+++ fatal_breath_server/app/Http/Controllers/SensorController.php
@@ -22,4 +22,22 @@ class SensorController extends Controller
 
         return response()->json(['message' => 'Sensor record created successfully'], 201);
     }
+
+    public function updateLevel(Request $request, $id)
+    {
+        $request->validate([
+            'co_level' => 'required|numeric',
+        ]);
+
+        $sensor = Sensor::find($id);
+
+        if (!$sensor) {
+            return response()->json(['message' => 'Sensor record not found'], 404);
+        }
+
+        $sensor->co_level = $request->input('co_level');
+        $sensor->save();
+
+        return response()->json(['message' => 'CO level updated successfully']);
+    }
 }
