246df8adc7291ad69b04ee249fbb410dfe6ff8b9
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-20 21:12:40 +0300

    Server <> edited the updateLevel api

--- fatal_breath_server/app/Http/Controllers/SensorController.php
+++ fatal_breath_server/app/Http/Controllers/SensorController.php
@@ -33,16 +33,20 @@ class SensorController extends Controller
     public function updateLevel(Request $request)
     {
         $request->validate([
+            'room_id' => 'required|exists:rooms,id',
             'co_level' => 'required|numeric',
         ]);
 
-        $sensor = Sensor::find($request->id);
+        $roomId = $request->input('room_id');
+        $coLevel = $request->input('co_level');
+
+        $sensor = Sensor::where('room_id', $roomId)->first();
 
         if (!$sensor) {
             return response()->json(['message' => 'Sensor record not found'], 404);
         }
 
-        $sensor->co_level = $request->input('co_level');
+        $sensor->co_level = $coLevel;
         $sensor->save();
 
         return response()->json(['message' => 'CO level updated successfully']);
