1f89645f007bf63062d30f1c60db154efbe66009
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-20 23:07:28 +0300

    Arduino <> initialized the needed variables

--- fatal_breath_arduino/main.cpp
+++ fatal_breath_arduino/main.cpp
@@ -1,2 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+
+const char *ssid = "AMAN";
+const char *password = "Aliz1966";
+const char *serverAddress = "http://192.168.1.5:8000";
+
+int sensorPin = A0;
+int coValue = 0;
+
+int roomId = 1;
+
+WiFiClient client;
+HTTPClient http;
