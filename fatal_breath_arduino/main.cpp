2678c254416eeb3560930fd6bdcb7256fb7e8fac
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-20 23:11:40 +0300

    Arduino <> added the initial value of the co

--- fatal_breath_arduino/main.cpp
+++ fatal_breath_arduino/main.cpp
@@ -5,6 +5,7 @@ const char *ssid = "AMAN";
 const char *password = "Aliz1966";
 const char *serverAddress = "http://192.168.1.5:8000";
 
+float lastCoPercentage = 0.0;
 int sensorPin = A0;
 int coValue = 0;
 
