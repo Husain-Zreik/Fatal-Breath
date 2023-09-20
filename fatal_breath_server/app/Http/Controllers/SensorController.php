62737faeab17bcf6d8401d1c163de2579997a5dc
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-20 20:29:17 +0300

    Server <> created connect api

--- fatal_breath_server/app/Http/Controllers/SensorController.php
+++ fatal_breath_server/app/Http/Controllers/SensorController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sensor;
 use Illuminate\Http\Request;
 
 class SensorController extends Controller
 {
-    //
+    public function connect(Request $request)
+    {
+        $request->validate([
+            'room_id' => 'required|exists:rooms,id',
+            'co_level' => 'required|numeric',
+        ]);
+
+        $sensor = new Sensor([
+            'room_id' => $request->input('room_id'),
+            'co_level' => $request->input('co_level'),
+        ]);
+        $sensor->save();
+
+        return response()->json(['message' => 'Sensor record created successfully'], 201);
+    }
 }
