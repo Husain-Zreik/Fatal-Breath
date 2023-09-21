b65aba3eb8074df4f87a56b1453cbb7516f874c8
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-21 14:34:25 +0300

    Frontend <> created the add box components

--- fatal_breath_frontend/lib/widgets/add.box.dart
+++ fatal_breath_frontend/lib/widgets/add.box.dart
@@ -1,10 +1,55 @@
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AddBox extends StatelessWidget {
-  const AddBox({Key? key}) : super(key: key);
+  const AddBox({Key? key, required this.label}) : super(key: key);
+
+  final String label;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 0, 20, 5),
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(
+            children: [
+              SizedBox(
+                  width: 60,
+                  child: Icon(
+                    Icons.add_circle_outline,
+                    size: 35,
+                    color: GlobalColors.mainColor,
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(label,
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
