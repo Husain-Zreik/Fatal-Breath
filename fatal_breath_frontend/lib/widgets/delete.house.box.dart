732116a65e9ccdc8c94fe61a72d1d9ba1fbce3ef
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-21 14:21:09 +0300

    Frontend <> created delete house box widget

---
+++ fatal_breath_frontend/lib/widgets/delete.house.box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeleteHouseBox extends StatelessWidget {
+  const DeleteHouseBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
