9180535157d39d13c532d628f0f3873542096396
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-21 14:24:05 +0300

    Frontend <> created add box widget

---
+++ fatal_breath_frontend/lib/widgets/add.box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddBox extends StatelessWidget {
+  const AddBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
