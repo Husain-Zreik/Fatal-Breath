6ba7ea6adc1b0ed0aa81861b577cf65ba4538dfc
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-21 15:25:19 +0300

    Frontend <> added the sensor to the room model

--- fatal_breath_frontend/lib/models/room.model.dart
+++ fatal_breath_frontend/lib/models/room.model.dart
@@ -1,9 +1,12 @@
+import 'package:fatal_breath_frontend/models/sensor.model.dart';
+
 class Room {
   final int id;
   final int houseId;
   final String type;
   final String name;
   final bool hasSensor;
+  final Sensor? sensor;
 
   Room({
     required this.id,
@@ -11,15 +14,24 @@ class Room {
     required this.type,
     required this.name,
     required this.hasSensor,
+    this.sensor,
   });
 
   factory Room.fromJson(Map<String, dynamic> json) {
+    final sensorJson = json['sensors'];
+    Sensor? roomSensor;
+
+    if (sensorJson != null) {
+      roomSensor = Sensor.fromJson(sensorJson);
+    }
+
     return Room(
       id: json['id'],
       houseId: json['house_id'],
       type: json['type'],
       name: json['name'],
       hasSensor: json['hasSensor'],
+      sensor: roomSensor,
     );
   }
 }
