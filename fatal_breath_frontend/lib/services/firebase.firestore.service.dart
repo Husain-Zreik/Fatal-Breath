ffeda97eb3201458ff09493d72d17391a2405845
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-23 17:51:51 +0300

    Frontend <> created the firebase services file

---
+++ fatal_breath_frontend/lib/services/firebase.firestore.service.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:fatal_breath_frontend/models/message.model.dart';
+
+class FirebaseFirestoreService {
+  static final firestore = FirebaseFirestore.instance;
+
+  static Future<void> addTextMessage({
+    required String content,
+    required String receiverId,
+  }) async {
+    final message = Message(
+      content: content,
+      sentTime: DateTime.now(),
+      receiverId: receiverId,
+      senderId: FirebaseAuth.instance.currentUser!.uid,
+    );
+    await _addMessageToChat(receiverId, message);
+  }
+
+  static Future<void> _addMessageToChat(
+    String receiverId,
+    Message message,
+  ) async {
+    await firestore
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('chat')
+        .doc(receiverId)
+        .collection('messages')
+        .add(message.toJson());
+
+    await firestore
+        .collection('users')
+        .doc(receiverId)
+        .collection('chat')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('messages')
+        .add(message.toJson());
+  }
+}
