8d0857859ccab79adf047d7aa67137f97da98c35
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-23 21:52:11 +0300

    Frontend <> edited the messages style

--- fatal_breath_frontend/lib/widgets/message.bubble.dart
+++ fatal_breath_frontend/lib/widgets/message.bubble.dart
@@ -1,4 +1,5 @@
 import 'package:fatal_breath_frontend/models/message.model.dart';
+import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:flutter/material.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -13,86 +14,49 @@ class MessageBubble extends StatelessWidget {
   final Message message;
 
   @override
-  Widget build(BuildContext context) => Align(
+  Widget build(BuildContext context) => Container(
         alignment: isMe ? Alignment.topLeft : Alignment.topRight,
         child: Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.red[200] : Colors.grey,
-            borderRadius: isMe
-                ? const BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
-                    topLeft: Radius.circular(30),
-                  )
-                : const BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    bottomLeft: Radius.circular(30),
-                    topLeft: Radius.circular(30),
+          constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.80),
+          child: Container(
+            decoration: BoxDecoration(
+              color: isMe
+                  ? const Color.fromARGB(255, 161, 0, 0)
+                  : GlobalColors.mainColor,
+              borderRadius: isMe
+                  ? const BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                      topLeft: Radius.circular(15),
+                    )
+                  : const BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                      topLeft: Radius.circular(15),
+                    ),
+            ),
+            margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 15),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment:
+                  isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+              children: [
+                Text(message.content,
+                    style: const TextStyle(color: Colors.white, fontSize: 15)),
+                const SizedBox(height: 5),
+                Text(
+                  timeago.format(message.sentTime),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
                   ),
-          ),
-          margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
-            children: [
-              Text(message.content,
-                  style: const TextStyle(color: Colors.white)),
-              const SizedBox(height: 5),
-              Text(
-                timeago.format(message.sentTime),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
-
-  // _chatBubble(Message message, bool isMe) {
-  //   if (!isMe) {
-  //     return Container(
-  //       alignment: Alignment.topLeft,
-  //       child: Container(
-  //         constraints: BoxConstraints(
-  //             maxWidth: MediaQuery.of(context).size.width * 0.70),
-  //         child: Container(
-  //             padding: const EdgeInsets.all(10),
-  //             margin: const EdgeInsets.symmetric(vertical: 10),
-  //             decoration: const BoxDecoration(
-  //               color: Colors.white,
-  //               borderRadius: BorderRadius.only(
-  //                   topLeft: Radius.zero,
-  //                   topRight: Radius.circular(20),
-  //                   bottomLeft: Radius.circular(20),
-  //                   bottomRight: Radius.circular(20)),
-  //             ),
-  //             child: Text(message.content)),
-  //       ),
-  //     );
-  //   } else {
-  //     return Container(
-  //       alignment: Alignment.topRight,
-  //       child: Container(
-  //         constraints: BoxConstraints(
-  //             maxWidth: MediaQuery.of(context).size.width * 0.70),
-  //         child: Container(
-  //             padding: const EdgeInsets.all(10),
-  //             margin: const EdgeInsets.symmetric(vertical: 10),
-  //             decoration: const BoxDecoration(
-  //               color: Colors.white,
-  //               borderRadius: BorderRadius.only(
-  //                   topRight: Radius.zero,
-  //                   topLeft: Radius.circular(20),
-  //                   bottomLeft: Radius.circular(20),
-  //                   bottomRight: Radius.circular(20)),
-  //             ),
-  //             child: Text(message.content)),
-  //       ),
-  //     );
-  //   }
-  // }
 }
