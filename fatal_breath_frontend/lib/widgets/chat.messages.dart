584219f2fd48e6d8650e2e44b236e23a2699201d
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-23 17:51:03 +0300

    Frontend <> edited the messages widget name and content

---
+++ fatal_breath_frontend/lib/widgets/chat.messages.dart
@@ -0,0 +1,28 @@
+import 'package:fatal_breath_frontend/providers/firebase.provider.dart';
+import 'package:fatal_breath_frontend/widgets/message.bubble.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key, required this.receiverId});
+  final String receiverId;
+
+  @override
+  Widget build(BuildContext context) => Consumer<FirebaseProvider>(
+        builder: (context, value, child) => Expanded(
+          child: ListView.builder(
+            controller: Provider.of<FirebaseProvider>(context, listen: false)
+                .scrollController,
+            itemCount: value.messages.length,
+            itemBuilder: (context, index) {
+              final isMe = receiverId != value.messages[index].senderId;
+
+              return MessageBubble(
+                isMe: isMe,
+                message: value.messages[index],
+              );
+            },
+          ),
+        ),
+      );
+}
