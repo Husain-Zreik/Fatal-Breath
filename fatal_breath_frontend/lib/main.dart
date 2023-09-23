b5c79afe3defb89d6fc66d67d669034aa464abe7
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-23 23:32:52 +0300

    Frontend <> removed routes

--- fatal_breath_frontend/lib/main.dart
+++ fatal_breath_frontend/lib/main.dart
@@ -5,11 +5,6 @@ import 'package:fatal_breath_frontend/providers/firebase.provider.dart';
 import 'package:fatal_breath_frontend/providers/house.provider.dart';
 import 'package:fatal_breath_frontend/providers/room.provider.dart';
 import 'package:fatal_breath_frontend/providers/user.provider.dart';
-import 'package:fatal_breath_frontend/screens/empty/home.empty.state.screen.dart';
-import 'package:fatal_breath_frontend/screens/home/home.screen.dart';
-import 'package:fatal_breath_frontend/screens/register/login.screen.dart';
-import 'package:fatal_breath_frontend/screens/main.screen.dart';
-import 'package:fatal_breath_frontend/screens/register/signup.screen.dart';
 import 'package:fatal_breath_frontend/screens/register/splash.screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -31,8 +26,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-
   @override
   void initState() {
     super.initState();
@@ -94,25 +87,9 @@ class _MyAppState extends State<MyApp> {
           create: (context) => FirebaseProvider(),
         ),
       ],
-      child: GetMaterialApp(
+      child: const GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/login',
-        routes: {
-          "/splash": (context) => const SplashScreen(),
-          "/login": (context) => const LoginScreen(),
-          "/signup": (context) => const SignUpScreen(),
-          "/home_emptyState": (context) => const HomeEmptyStateScreen(),
-        },
-        getPages: [
-          GetPage(name: '/splash', page: () => const SplashScreen()),
-          GetPage(name: '/login', page: () => const LoginScreen()),
-          GetPage(name: '/signup', page: () => const SignUpScreen()),
-          GetPage(name: '/main', page: () => const MainScreen()),
-          GetPage(name: '/home', page: () => const HomeScreen()),
-          GetPage(
-              name: '/home_emptyState',
-              page: () => const HomeEmptyStateScreen()),
-        ],
+        home: SplashScreen(),
       ),
     );
   }
