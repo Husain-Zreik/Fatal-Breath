ad266ca4cb7b5eadd7d8575892472483226f77fe
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-23 17:52:26 +0300

    Frontend <> created the firebase provider

---
+++ fatal_breath_frontend/lib/providers/firebase.provider.dart
@@ -0,0 +1,33 @@
+import 'package:fatal_breath_frontend/models/message.model.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseProvider extends ChangeNotifier {
+  ScrollController scrollController = ScrollController();
+
+  List<Message> messages = [];
+
+  List<Message> getMessages(String receiverId) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('chat')
+        .doc(receiverId)
+        .collection('messages')
+        .orderBy('sentTime', descending: false)
+        .snapshots(includeMetadataChanges: true)
+        .listen((messages) {
+      this.messages =
+          messages.docs.map((doc) => Message.fromJson(doc.data())).toList();
+      notifyListeners();
+
+      scrollDown();
+    });
+    return messages;
+  }
+
+  void scrollDown() => WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (scrollController.hasClients) {
+          scrollController.jumpTo(scrollController.position.maxScrollExtent);
+        }
+      });
+}
