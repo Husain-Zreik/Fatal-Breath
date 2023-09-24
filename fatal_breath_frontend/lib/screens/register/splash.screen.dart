25386754bafd2dac089fbd612cc197d531e47feb
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-24 13:37:09 +0300

    Frontend <> added the check logged in function to splash screen

--- fatal_breath_frontend/lib/screens/register/splash.screen.dart
+++ fatal_breath_frontend/lib/screens/register/splash.screen.dart
@@ -1,18 +1,61 @@
 import 'dart:async';
 
+import 'package:fatal_breath_frontend/providers/auth.provider.dart';
+import 'package:fatal_breath_frontend/providers/house.provider.dart';
+import 'package:fatal_breath_frontend/providers/user.provider.dart';
+import 'package:fatal_breath_frontend/screens/main.screen.dart';
 import 'package:fatal_breath_frontend/screens/register/login.screen.dart';
 import 'package:fatal_breath_frontend/utils/global.colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String? userType;
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToNextScreen();
+  }
+
+  Future<void> navigateToNextScreen() async {
+    final isValid = await loggedInChecker(context);
+
     Timer(const Duration(seconds: 3), () {
-      Get.to(const LoginScreen());
+      isValid == true
+          ? Get.off(() => const MainScreen())
+          : Get.off(() => const LoginScreen());
     });
+  }
+
+  Future<bool> loggedInChecker(context) async {
+    final isValid = await Provider.of<AuthProvider>(context, listen: false)
+        .checkTokenValidity();
+    if (isValid == true) {
+      await Provider.of<UserProvider>(context, listen: false).getUser(context);
+      userType = Provider.of<UserProvider>(context, listen: false).getUserType;
+
+      if (userType == "Manager") {
+        await Provider.of<HouseProvider>(context, listen: false)
+            .getAdminHouses();
+      } else {
+        await Provider.of<HouseProvider>(context, listen: false)
+            .getUserHouses();
+      }
+    }
+    return isValid;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: GlobalColors.mainColor,
       body: Center(
