c9ed451177f01490402ca94a4bb40a391eb09eb1
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-24 16:14:26 +0300

    Frontend <> added the isSearch to the users empty state

--- fatal_breath_frontend/lib/screens/empty/user.empty.state.screen.dart
+++ fatal_breath_frontend/lib/screens/empty/user.empty.state.screen.dart
@@ -2,18 +2,25 @@ import 'package:fatal_breath_frontend/utils/text.note.dart';
 import 'package:flutter/material.dart';
 
 class UserEmptyStateScreen extends StatelessWidget {
-  const UserEmptyStateScreen({Key? key, required this.text}) : super(key: key);
+  const UserEmptyStateScreen({Key? key, required this.text, this.isSearch})
+      : super(key: key);
 
   final String text;
+  final bool? isSearch;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset(
+              isSearch != null
+                  ? "assets/images/search_icon.png"
+                  : "assets/images/empty_house_icon.png",
+            ),
             TextNote(text: text),
           ],
         ),
