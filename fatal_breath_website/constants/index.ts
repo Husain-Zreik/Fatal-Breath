aecb31c7d08cb176e317e444c35cdf2f8d63cc0f
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-25 20:14:42 +0300

    Website <> created constants file with footerLinks

---
+++ fatal_breath_website/constants/index.ts
@@ -0,0 +1,16 @@
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Instagram", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
