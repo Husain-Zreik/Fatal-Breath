0e93bc2011535bee0b61b5b7e52717a4ddbdf6ef
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 01:56:54 +0300

    Website <> created the purpose component

---
+++ fatal_breath_website/components/Purpose.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+
+const Purpose = () => {
+
+    return (
+    <div className="bg-[#FFE3E3] " id="Purpose">
+    <div className="hero ">
+
+        <Image src="/alert.png" alt="alert" width={400} height={400} className="  object-contain" />
+
+        <div className="flex-1 pt-24 padding-x mb-20">
+            <h1 className="hero__title">
+            Purpose:
+            </h1>
+
+            <p className="hero__subtitle">
+            “Fatal Breathe” is an application built to reduce the death caused by the high amount of toxic gases such as CO in the house. 
+            </p>
+
+        </div>
+    </div>
+    </div>
+    );
+};
+
+export default Purpose;
