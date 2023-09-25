bf623eaa23275d926c80a125a85267eea7516dc9
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 01:56:14 +0300

    Website <> exported the components

--- fatal_breath_website/components/index.ts
+++ fatal_breath_website/components/index.ts
@@ -1,11 +1,17 @@
-import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Features from "./Features";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+import UserTypes from "./UserTypes";
+import Purpose from "./Purpose";
 
 export{
     Hero,
+    Features,
+    UserTypes,
     CustomButton,
+    Purpose,
     NavBar,
     Footer,
 }
