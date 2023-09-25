bff63a3d468c4501c161f9986323c44371d05607
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 02:01:45 +0300

    Website <> added the on hover on the baoxes

--- fatal_breath_website/components/Features.tsx
+++ fatal_breath_website/components/Features.tsx
@@ -3,20 +3,19 @@ import Image from "next/image";
 
 const Features = () => {
     return (
-        <div className="bg-[#FFE3E3] md:h-screen" id="Features">
+        <div className="bg-[#FFE3E3] md:h-screen">
             <div className="hero h-full flex flex-col justify-center items-center">
-                <div>
                 <Image src="/icons.png" alt="alert" width={400} height={400} className="object-contain" />
-                <Image src="/devs.png" alt="alert" width={400} height={400} className="object-contain" />
-
-                </div>
 
                 <div className="flex-1 padding-x">
                     <h1 className="hero__title mb-16">Features:</h1>
 
                     <div className="flex flex-wrap justify-center sm:items-center gap-3 mb-10">
                         {features.map((feature, index) => (
-                            <div key={index} className="bg-white rounded-lg shadow-md p-4 w-72">
+                            <div
+                                key={index}
+                                className="bg-white rounded-lg shadow-md p-4 w-72 transform hover:scale-105 transition-transform cursor-pointer"
+                            >
                                 <h2 className="text-xl font-bold mb-2">{feature.title}</h2>
                                 <p className="text-sm">{feature.description}</p>
                             </div>
