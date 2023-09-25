da44ff16dee2056abcd16db819bbf61b2b902602
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 01:55:53 +0300

    Website <> added the navigation buttons

--- fatal_breath_website/components/Hero.tsx
+++ fatal_breath_website/components/Hero.tsx
@@ -5,8 +5,8 @@ import { CustomButton } from ".";
 
 
 const Hero = () => {
-    const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
+    const handleScroll = (direction: string) => {
+    const nextSection = document.getElementById(direction);
 
     if (nextSection) {
         nextSection.scrollIntoView({ behavior: "smooth" });
@@ -14,27 +14,41 @@ const Hero = () => {
     };
 
     return (
+    <div className="bg-[#E3EEFF] ">
     <div className="hero ">
-        <div className="flex-1 pt-36 padding-x">
+        <div className="flex-1 pt-36 padding-x ">
             <h1 className="hero__title">
-            Fatal Breath: {<br/>} Your Guardian Angel Against Toxic Gases!
+            Fatal Breath: <br/> Your Guardian Angel Against Toxic Gases!
             </h1>
 
             <p className="hero__subtitle">
             Protect your family from the silent killer with Fatal Breath.
             </p>
 
-            <CustomButton
-                title="Explore"
-                containerStyles="bg-primary-blue bg-[#1C246C] text-white rounded-full mt-10"
-                handleClick={handleScroll}
-            />
+            <div className="flex justify-around flex-row items-center">
+                <CustomButton
+                            title="Purpose"
+                            containerStyles="bg-primary-blue bg-[#1C246C] text-white rounded-full mt-10"
+                            handleClick={()=>handleScroll("Purpose")}
+                />
+                <CustomButton
+                    title="Users"
+                    containerStyles="bg-primary-blue bg-[#1C246C] text-white rounded-full mt-10"
+                    handleClick={()=>handleScroll("User-Types")}
+                />
+                <CustomButton
+                    title="Features"
+                    containerStyles="bg-primary-blue bg-[#1C246C] text-white rounded-full mt-10"
+                    handleClick={()=>handleScroll("Features")}
+                />
+            </div>
         </div>
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="hero" fill className="object-contain" />
             </div>
         </div>
+    </div>
     </div>
     );
 };
