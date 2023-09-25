ef2c15d4f1ffd35dd8a0f215963e5d6c77370662
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 01:57:32 +0300

    Website <> added the features constant

--- fatal_breath_website/constants/index.ts
+++ fatal_breath_website/constants/index.ts
@@ -2,7 +2,7 @@ export const footerLinks = [
   {
     title: "About",
     links: [
-      { title: "How it works", url: "/" },
+      { title: "Main", url: "/" },
       { title: "Featured", url: "/" },
     ],
   },
@@ -14,3 +14,50 @@ export const footerLinks = [
     ],
   },
 ];
+
+export const features = [
+  {
+    title: "Monitor rooms in real time",
+    description: "See the CO level in each room of the houses you are a member of.",
+  },
+  {
+      title: "Communicate with users",
+      description: "Use the in-house chat feature to stay connected with your family and friends.",
+  },
+  {
+    title: "User-friendly Interface",
+    description: "An intuitive and easy-to-use interface for seamless navigation.",
+  },
+{
+    title: "Instant CO Alerts",
+    description: "Receive immediate alerts if CO levels rise above safe thresholds.",
+},
+{
+    title: "Multi-Room Control",
+    description: "Control and manage multiple rooms within your property.",
+},
+{
+    title: "House Connectivity",
+    description: "Connect your entire house to the application for comprehensive coverage.",
+},
+{
+    title: "User Management",
+    description: "Effortlessly add and remove users from your house for shared access.",
+  },
+  {
+    title: "CO Level Display",
+    description: "Easily check CO levels in any room to stay informed about your environment.",
+  },
+  {
+    title: "Emergency Chat",
+    description: "Chat with other users in the house to communicate during emergencies.",
+  },
+  // {
+    //     title: "AI-Powered Consultation",
+    //     description: "Get expert safety advice through AI-powered consultations.",
+    // },
+    // {
+    //     title: "Get help with safety procedures:",
+    //     description: "Fatal Breath's AI-powered consultation feature can answer your questions about safety procedures and help you stay safe in an emergency.",
+    // },
+  ];
