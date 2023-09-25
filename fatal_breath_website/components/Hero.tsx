5591fe5b044d89a03caacefa4cfdec4fb5839269
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-25 15:45:29 +0300

    Website <> edited the hero page

--- fatal_breath_website/components/Hero.tsx
+++ fatal_breath_website/components/Hero.tsx
@@ -1,9 +1,42 @@
-import Image from 'next/image'
+"use client";
 
-function Hero() {
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+
+const Hero = () => {
+    const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+        nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+    };
+
     return (
-        <div>Hero</div>
-    )
-}
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+            Fatal Breath: {<br/>} Your Guardian Angel Against Toxic Gases!
+            </h1>
 
-export default Hero
+            <p className="hero__subtitle">
+            Protect your family from the silent killer with Fatal Breath.
+            </p>
+
+            <CustomButton
+                title="Explore"
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+        </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="hero" fill className="object-contain" />
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Hero;
