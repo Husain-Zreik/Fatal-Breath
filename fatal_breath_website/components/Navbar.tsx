d0e292c98015e2f6627e74a88c51f93c7b5f1ef7
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-26 21:50:54 +0300

    Website <> added the download functionality

--- fatal_breath_website/components/Navbar.tsx
+++ fatal_breath_website/components/Navbar.tsx
@@ -1,28 +1,37 @@
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
 
 import CustomButton from "./CustomButton";
 
-const NavBar = () => (
-  <header className='w-full  absolute z-10 '>
-    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
-        <Image
-          src='/logo.svg'
-          alt='logo'
-          width={250}
-          height={18}
-          className='object-contain'
-        />
-      </Link>
+const NavBar = () => {
+  const handleDownloadClick = () => {
+    window.location.href = 'https://play.google.com';
+  };
 
-      <CustomButton
-        title='Download'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-      />
-    </nav>
-  </header>
-);
+  return (
+    <header className='w-full  absolute z-10 '>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+        <Link href='/' className='flex justify-center items-center'>
+          <Image
+            src='/logo.svg'
+            alt='logo'
+            width={250}
+            height={18}
+            className='object-contain'
+          />
+        </Link>
+
+        <CustomButton
+          title='Download'
+          btnType='button'
+          containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+          handleClick={handleDownloadClick}
+        />
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
