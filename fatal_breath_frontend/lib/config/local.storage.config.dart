78092e315806ec18500789badefd6ff620d1141e
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-09-28 13:59:25 +0300

    Frontend <> cleaned the local storage file

--- fatal_breath_frontend/lib/config/local.storage.config.dart
+++ fatal_breath_frontend/lib/config/local.storage.config.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: invalid_use_of_visible_for_testing_member
-
 import "package:fatal_breath_frontend/enums/local.types.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
@@ -8,8 +6,6 @@ Future<void> setlocal({
   required String key,
   required dynamic value,
 }) async {
-  // SharedPreferences.setMockInitialValues({key: value});
-
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
   if (type == LocalTypes.Int) {
@@ -27,8 +23,6 @@ dynamic getLocal({
   required LocalTypes type,
   required String key,
 }) async {
-  // SharedPreferences.setMockInitialValues({});
-
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
   dynamic result;
