e1d74a3204bcdc459cdc849b7903b5887171b0f8
Husain-Zreik <husain.zreik1@gmail.com>
2025-06-15 22:00:26 +0300

    Arduino <> remove the sending notification from the sensor

--- fatal_breath_arduino/fatal_breath_arduino.ino
+++ fatal_breath_arduino/fatal_breath_arduino.ino
@@ -3,10 +3,8 @@
 
 const char *ssid = "AMAN";
 const char *password = "Aliz1966";
-// const char *serverAddress = "http://192.168.1.5:8000";
-const char *serverAddress = "http://54.75.66.192";
-
-const char *deviceFCMToken = "dIg7CJYrRNeiBhJ4IjXpcP:APA91bGSnwRy4OFyRttKizzJjQWSMEVa_pekfbhDl3iZ554Ty-idkLXuc9jtyANxLPJjwVzrHmLw5s5utBnw3xRM-4qmXNJ545DKVHmBsnaQAW5j2iRbT-6isLxxsffp1Zz8ckuBNl0J";
+const char *serverAddress = "http://192.168.1.10:8000";
+// const char *serverAddress = "http://54.75.66.192";
 
 int lastCoPercentage = 0.0;
 int previousCoPercentage = 0.0;
@@ -48,17 +46,9 @@ void loop()
     if (abs(coPercentage - lastCoPercentage) >= 5.0)
     {
         updateLevel(roomId, coPercentage);
-        lastCoPercentage = coPercentage;
-    }
-    if (coPercentage > 40.0 && previousCoPercentage <= 40.0)
-    {
-        sendFCMNotification(coPercentage, "CO Level Alert", "CO Level is sensitive it crossed 40% and now it is ");
+        lastCoPercentage = coPercentage;    
     }
 
-    if (coPercentage > 70.0)
-    {
-        sendFCMNotification(coPercentage, "CO Level Alert", "CO Level is dangerous it crossed 70% and now it is ");
-    }
     previousCoPercentage = coPercentage;
 
     delay(5000);
@@ -107,34 +97,6 @@ void updateLevel(int roomId, int coPercentage)
     else
     {
         Serial.println("Failed to update CO level");
-    }
-
-    http.end();
-}
-
-void sendFCMNotification(int coPercentage, const String &title, const String &body)
-{
-    String serverEndpoint = "/api/send-notification";
-    String jsonPayload = "{\"notification\":{"
-                         "\"title\":\"" +
-                         title + "\","
-                                 "\"body\":\"" +
-                         body + String(coPercentage) + "%\""
-                                                       "},"
-                                                       "\"to\":\"" +
-                         String(deviceFCMToken) + "\"}";
-
-    http.begin(client, serverAddress + serverEndpoint);
-    http.addHeader("Content-Type", "application/json");
-
-    int httpResponseCode = http.POST(jsonPayload);
-    if (httpResponseCode == 200)
-    {
-        Serial.println("Notification sent successfully");
-    }
-    else
-    {
-        Serial.println("Failed to send notification");
     }
 
     http.end();
